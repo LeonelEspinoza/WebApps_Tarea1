9bb411f6d1cf035db086a6e471088511f32887f4
LeonelEspinoza <leonel.espinoza@ug.uchile.cl>
2023-04-11 22:13:45 -0400

    no hay errores segun w3 en html

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # WebApps_Tarea1
 Tarea 1 Desarrollo de Aplicaciones Web CC5002-1
+
+Hay fotos que daban libre uso a condicion de tener un link al sitio original. Es por eso que algunas imagenes llevan a una pagina externa.
+Todas las fotos fueron scadas de sitios que daban libre uso de ellas.
+
+La primera fila de donaciones lleva a informacion-donacion.html
+
+La ultima fila de pedidos lleva a informacion-pedido.html
---
+++ js/comunas.js
@@ -0,0 +1,37 @@
+import region_comuna from "../region_comuna.json"
+
+const regionselect = document.getElementById("region");
+const comunaselect = document.getElementById("comuna");
+
+regionselect.addEventListener("change", event => {
+    comunaselect.innerHTML = "<option selected disabled>Elegir comuna</option>";
+
+    const regionvalue = event.taget.value;
+
+ const region = region_comuna.find(region => region.id == regionvalue);
+    
+    region.comunas.forEach(comuna => {
+        const option = document.createElement("option");
+        option.value = comuna.id;
+        option.textContent = comuna.nombre;
+        comunaselect.appendChild(option);
+    });
+});
+
+
+
+
+//Mostrar las comunas de la región seleccionada
+regionSelect.addEventListener("change", e => {
+    // Limpiar las opciones del select de comunas
+    comunaSelect.innerHTML = "<option selected disabled>Seleccione una comuna</option>";
+
+    const regionId = e.target.value;
+    const region = regiones.find(region => region.id == regionId);
+    region.comunas.forEach(comuna => {
+        const option = document.createElement("option");
+        option.value = comuna.id;
+        option.textContent = comuna.nombre;
+        comunaSelect.appendChild(option);
+    });
+});
