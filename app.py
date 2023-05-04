024d771425461f1850e0d38d7a542982936cd25a
LeonelEspinoza <leonel.espinoza@ug.uchile.cl>
2023-05-04 16:58:38 -0400

    Refactoring x2

--- app.py
+++ app.py
@@ -10,6 +10,26 @@ app = Flask(__name__)
 def index():
     return render_template("inicio.html")
 
+#http://localhost/agregar-donacion
+@app.route("/agregar-donacion")
+def add_donation():
+    return render_template("agregar-donacion.html")
+
+#http://localhost/agregar-pedido
+@app.route("/agregar-pedido")
+def add_request():
+    return render_template("agregar-pedido.html")
+
+#http://localhost/ver-donaciones
+@app.route("/ver-donaciones")
+def donations():
+    return render_template("ver-donaciones.html")
+
+#http://localhost/ver-pedidos
+@app.route("/ver-pedidos")
+def requests():
+    return render_template("ver-pedidos.html")
+
 #http://localhost/login
 @app.route("/login")
 def login():
