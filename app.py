cdc859e0977cedf653b9b16dfe10d13c1985a774
LeonelEspinoza <leonel.espinoza@ug.uchile.cl>
2023-05-06 15:27:15 -0400

    Fix specific information htmls and minor fixes on 'app.py','ver-*.html'

--- app.py
+++ app.py
@@ -27,40 +27,38 @@ def add_donation():
         nombre = request.form['nombre']
         email = request.form['email']
         celular = request.form['celular']
-    if (validate_donation(
-        region,
-        comuna,
-        calle_numero,
-        tipo_pedido,
-        cantidad,
-        fecha,
-        descripcion,
-        condiciones_retiro,
-        foto_donacion,
-        nombre,
-        email,
-        celular
-        )):
-        return redirect(url_for('index'))
-    else:
-        error='Form invalido'
+    #validation=validate_donation(region,comuna,calle_numero,tipo_pedido,cantidad,fecha,descripcion,condiciones_retiro,foto_donacion,nombre,email,celular)
+    #if (validation):
+    #    return redirect(url_for('index'))
+    #else:
+    #    error='Form invalido'
     
-    return render_template("agregar-donacion.html")
+    return render_template("forms/agregar-donacion.html")
 
 #http://localhost/agregar-pedido
 @app.route("/agregar-pedido")
 def add_request():
-    return render_template("agregar-pedido.html")
+    return render_template("forms/agregar-pedido.html")
 
 #http://localhost/ver-donaciones
 @app.route("/ver-donaciones")
 def donations():
-    return render_template("ver-donaciones.html")
+    return render_template("tables/ver-donaciones.html")
 
 #http://localhost/ver-pedidos
 @app.route("/ver-pedidos")
 def requests():
-    return render_template("ver-pedidos.html")
+    return render_template("tables/ver-pedidos.html")
+
+#http://localhost/informacion-donacion
+@app.route("/informacion-donacion")
+def donation_info():
+    return render_template("specific_info/informacion-donacion.html")
+
+#http://localhost/informacion-pedido
+@app.route("/informacion-pedido")
+def request_info():
+    return render_template("specific_info/informacion-pedido.html")
 
 #http://localhost/login
 @app.route("/login")
