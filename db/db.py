ad8e1523f2023076981741c6db883773af93a370
LeonelEspinoza <leonel.espinoza@ug.uchile.cl>
2023-05-07 17:07:24 -0400

    Add database conection functions

--- db/db.py
+++ db/db.py
@@ -1,10 +1,8 @@
 import pymysql
-import json
 
 DB_NAME = "tarea2"
-DB_NAME = "confesiones"
-DB_USERNAME = "dbadmin"
-DB_PASSWORD = "dbadmin"
+DB_USERNAME = "root"
+DB_PASSWORD = "liaes.160729"
 DB_HOST = "localhost"
 DB_PORT = 3306
 DB_CHARSET = "utf8"
@@ -23,3 +21,44 @@ def get_conn():
 		charset=DB_CHARSET
 	)
 	return conn
+
+def get_donations(page):
+	conn = get_conn()
+	cursor = conn.cursor()
+	cursor.execute("SELECT id, comuna_id, calle_numero, tipo, cantidad, fecha_disponibilidad, descripcion, condiciones_retirar, nombre, email, celular FROM donacion ORDER BY id DESC LIMIT %s,%s;",((page-1)*5,page*5))
+	donations = cursor.fetchall()
+	return donations
+
+def create_donation(a,b,c,d,e,f,g,h,i,j):
+	conn = get_conn()
+	cursor = conn.cursor()
+	cursor.execute("INSERT INTO donacion (comuna_id, calle_numero, tipo, cantidad, fecha_disponibilidad, descripcion, condiciones_retirar, nombre, email, celular) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);",(a,b,c,d,e,f,g,h,i,j))
+	conn.commit()
+
+def get_donation_by_id(id):
+	conn = get_conn()
+	cursor = conn.cursor()
+	cursor.execute("SELECT id, comuna_id, calle_numero, tipo, cantidad, fecha_disponibilidad, descripcion, condiciones_retirar, nombre, email, celular FROM donacion WHERE id=%s;", (id,))
+	donation = cursor.fetchone()
+	return donation
+
+def get_requests(page):
+	conn = get_conn()
+	cursor = conn.cursor()
+	cursor.execute("SELECT id, comuna_id, tipo, descripcion, cantidad, nombre_solicitante, email_solicitante, celular_solicitante FROM pedido ORDER BY id DESC LIMIT %s,%s;",((page-1)*5,page*5))
+	requests = cursor.fetchall()
+	return requests
+
+
+def create_request(a,b,c,d,e,f,g):
+	conn = get_conn()
+	cursor = conn.cursor()
+	cursor.execute("INSERT INTO pedido (comuna_id, tipo, descripcion, cantidad, nombre_solicitante, email_solicitante, celular_solicitante) VALUES (%s, %s, %s, %s, %s, %s, %s);",(a,b,c,d,e,f,g))
+	conn.commit()
+
+def get_request_by_id(id):
+	conn = get_conn()
+	cursor = conn.cursor()
+	cursor.execute("SELECT id, comuna_id, tipo, descripcion, cantidad, nombre_solicitante, email_solicitante, celular_solicitante FROM pedido WHERE id=%s;", (id,))
+	request = cursor.fetchone()
+	return request
